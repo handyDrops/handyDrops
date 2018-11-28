0d1a59fb3e339b77c0d723fdbc2a935a9db51f72
cocobilbao <cocobilbao@gmail.com>
2018-11-28 18:10:00 +0100

    email style

--- app.js
+++ app.js
@@ -32,16 +32,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-// Express View engine setup
-
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(cookieParser());  
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
